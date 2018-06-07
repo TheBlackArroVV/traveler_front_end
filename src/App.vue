abb5fdf73079e9668c12027c076313e7cf4d6e81
TheBlackArroVV <theblackarrovv@gmail.com>
2018-06-07 19:04:53 +0300

    maybe i breaked something, but i will repear, i think its more important

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,8 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<style lang="scss">
+  // @import './styles/custom-bootstrap.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>

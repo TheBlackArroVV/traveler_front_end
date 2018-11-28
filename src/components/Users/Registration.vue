4dca8835a78b00a0ae68ab5120344f6655fdf06b
Vasya <theblackarrovv@gmail.com>
2018-11-28 15:07:52 +0200

    fix authentication

--- src/components/Users/Registration.vue
+++ src/components/Users/Registration.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <h3>{{ errors.length > 0 ? errors : '' }}</h3>
     <h3>{{ registred ? 'You was succesfuly registred' : '' }}</h3>
-    <form class="new_user_form" v-on:submit="register()">
+    <form class="new_user_form" v-on:submit.prevent="register()">
       <div class="col-sm-4">
         <input type="email" v-model="email" placeholder="your email" class="form-control form-control-lg"><br>
         <input type="password" v-model="password" placeholder="your password" class="form-control form-control-lg"><br>
@@ -44,6 +44,8 @@ export default {
           location.href = '/'
         })
         .catch(e => {
+          this.errors = []
+          console.log(e.response)
           this.errors.push(e.response.data)
           this.registred = false
         })

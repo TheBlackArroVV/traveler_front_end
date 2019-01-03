80a09a69ae2d4204d351ed026fa2b27aeffb6760
TheBlackArroVV <theblackarrovv@gmail.com>
2019-01-03 15:28:36 +0200

    messages in topics

--- src/components/Forum/Show.vue
+++ src/components/Forum/Show.vue
@@ -2,8 +2,19 @@
   <div class="topic">
     <h1>{{ topic.title }}</h1>
     <h4>{{ topic.description }}</h4>
-      <!-- <a :href="'/#/posts/' + post.id + '/edit'">Edit this Post</a><br> -->
-      <a href="/#/forum" @click="deleteTopic()">Delete this topic</a>
+    <a href="/#/forum" @click="deleteTopic()">Delete this topic</a>
+    <ul class="list-group">
+        <li v-for="message in messages" :key = "message.id" class="list-group-item col-sm-4"><br/>
+          <div>
+            {{ message.user_name }}<br/>
+            {{ message.text }}
+          </div>
+        </li>
+    </ul>
+    <form class="new_message_form" v-on:submit.prevent="createMessage()">
+      <input type="text" v-model="text">
+      <button type="submit" name="button">Create message</button>
+    </form>
       <router-view></router-view>
   </div>
 </template>
@@ -16,7 +27,9 @@ export default {
   data () {
     return {
       topic: '',
-      errors: []
+      errors: [],
+      messages: [],
+      text: ''
     }
   },
 
@@ -29,12 +42,30 @@ export default {
       .catch(e => {
         this.errors.push(e)
       })
+      this.getMessages()
   },
 
   methods: {
+    getMessages () {
+      axios.get(`http://localhost:3000/api/v1/topics/` + this.$route.params.id + `/messages`)
+        .then(response => {
+          this.messages = response.data
+        })
+    },
     deleteTopic () {
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$session.get('jwt')
       axios.delete(`http://localhost:3000/api/v1/topics/` + this.$route.params.id)
+    },
+    createMessage () {
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$session.get('jwt')
+      axios.post(`http://localhost:3000/api/v1/topics/` + this.$route.params.id + `/messages`, {
+        message: {
+          text: this.text
+        }
+      })
+        .then(() => {
+          this.getMessages()
+        })
     }
   }
 }

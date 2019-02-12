6116cdbc09ab05edb0c482df8f9462e60a15e1df
TheBlackArroVV <theblackarrovv@gmail.com>
2019-02-12 16:07:51 +0200

    add recommendations list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,6 +24,9 @@
                 <li class="nav-item">
                   <a class="nav-link" href="/#/trips">Trips</a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/#/sights/recommendations">Recommendations</a>
+                </li>
             </ul>
           <ul class="navbar-nav mr-auto" v-else>
                 <li class="nav-item">
--- src/components/Sights/Show.vue
+++ src/components/Sights/Show.vue
@@ -55,29 +55,20 @@ export default {
     },
     likeSight () {
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$session.get('jwt')
-      axios.patch(`http://localhost:3000/api/v1/sights/` + this.$route.params.id + '/like') 
+      axios.patch(`http://localhost:3000/api/v1/sights/` + this.$route.params.id + '/like')
     },
     dislikeSight () {
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$session.get('jwt')
-      axios.patch(`http://localhost:3000/api/v1/sights/` + this.$route.params.id + '/dislike') 
+      axios.patch(`http://localhost:3000/api/v1/sights/` + this.$route.params.id + '/dislike')
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
 }
-/* ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-} */
 a {
   color: #42b983;
 }
